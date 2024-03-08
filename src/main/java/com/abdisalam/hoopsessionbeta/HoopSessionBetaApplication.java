a47230b8cba0c2b325e7a3263ddf8a57d2e0d06e
Abdisalam kadir <abdis@MacBook-Air.hsd1.wa.comcast.net>
2024-03-07 18:09:10 -0800

    Initialized a spring boot application

---
+++ src/main/java/com/abdisalam/hoopsessionbeta/HoopSessionBetaApplication.java
@@ -0,0 +1,13 @@
+package com.abdisalam.hoopsessionbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopSessionBetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoopSessionBetaApplication.class, args);
+    }
+
+}
